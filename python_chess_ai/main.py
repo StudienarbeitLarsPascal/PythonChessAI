9ff5ae52b54c64a2bf3ac57e37ae6d9d4b0e47f1
Lars Dittert <l.dittert@fensterbau-rueckriegel.de>
2019-01-12 20:04:24 +0100

    changed argparse arguments

--- python_chess_ai/main.py
+++ python_chess_ai/main.py
@@ -7,6 +7,7 @@
 # This file is the main file which starts the chess program and calls the elementary methods
 #
 
+import argparse
 from settings import terminal, gui
 from chess_master import ChessMaster
 from misc.tools import Tools
@@ -14,16 +15,24 @@ from player import ai, api, dummy, user
 
 USER_INPUT_MESSAGE = "Enter 0 for terminal and 1 for GUI: "
 
-
+# [terminal|GUI] [Playername] [User|AI|API|Dummy] [Playername2] [User|AI|API|Dummy] 
+parser = argparse.ArgumentParser()
+ui_group = parser.add_mutually_exclusive_group()
+ui_group.add_argument("-t", "--terminal", help="starts the terminal ui", action="store_true")
+ui_group.add_argument("-g", "--gui", help="starts the GUI", action="store_true")
+args = parser.parse_args()
 def main():
-    ui_status = Tools.check_legal_input_int(range(2), USER_INPUT_MESSAGE)
-    settings_ui = ui_switcher.get(ui_status).Settings()
+    if args.gui or (not (args.terminal) and not (args.gui)):
+        ui_status = 1
+    elif args.terminal:
+        ui_status = 0
 
+    settings_ui = ui_switcher.get(ui_status).Settings()
     player_settings = settings_ui.interrogate_settings()
     players = []
     for player_setting in player_settings:
-        type = type_switcher.get(player_setting.type)
-        players.append(type.Player(player_setting.num, player_setting.name, ui_status, player_setting.difficulty))
+       type = type_switcher.get(player_setting.type)
+       players.append(type.Player(player_setting.num, player_setting.name, ui_status, player_setting.difficulty))
 
     chess_master = ChessMaster()
     chess_master.start_chess_game(players)
