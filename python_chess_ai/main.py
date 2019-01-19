f1b1b6701254a644f47a346ac5cf5b1b87ceb488
Lars Dittert <l.dittert@fensterbau-rueckriegel.de>
2019-01-20 00:01:32 +0100

    optimized argparse arguments and add vscode settings to gitignore

--- python_chess_ai/main.py
+++ python_chess_ai/main.py
@@ -13,34 +13,35 @@ from chess_master import ChessMaster
 from misc.tools import Tools
 from player import ai, api, dummy, user
 
-USER_INPUT_MESSAGE = "Enter 0 for terminal and 1 for GUI: "
-
-# [terminal|GUI] [Playername] [User|AI|API|Dummy] [Playername2] [User|AI|API|Dummy] 
+# usage argument parser: [-h][-t | -g][-v]
 parser = argparse.ArgumentParser()
 ui_group = parser.add_mutually_exclusive_group()
 ui_group.add_argument("-t", "--terminal", help="starts the terminal ui", action="store_true")
 ui_group.add_argument("-g", "--gui", help="starts the GUI", action="store_true")
+parser.add_argument("-v", "--version", help="print the version number and exit", action="store_true")
 args = parser.parse_args()
 def main():
     if args.gui or (not (args.terminal) and not (args.gui)):
         ui_status = 1
     elif args.terminal:
         ui_status = 0
+    elif args.version:
+        print("ToDo Version")
 
     settings_ui = ui_switcher.get(ui_status).Settings()
     player_settings = settings_ui.interrogate_settings()
     players = []
     for player_setting in player_settings:
-       type = type_switcher.get(player_setting.type)
-       players.append(type.Player(player_setting.num, player_setting.name, ui_status, player_setting.difficulty))
+        type = type_switcher.get(player_setting.type)
+        players.append(type.Player(player_setting.num, player_setting.name, ui_status, player_setting.difficulty))
 
     chess_master = ChessMaster()
     chess_master.start_chess_game(players)
 
 ui_switcher = {
-        0: terminal,
-        1: gui
-}
+    0: terminal,
+    1: gui
+}       
 
 type_switcher = {
     1: user,
