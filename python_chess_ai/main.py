1e61ffab935b982d2aa17603095ad9a0bedd10e3
Lars Dittert <l.dittert@fensterbau-rueckriegel.de>
2019-03-29 01:43:55 +0100

    refactored code after review and added bash script for auto-play

--- python_chess_ai/main.py
+++ python_chess_ai/main.py
@@ -17,14 +17,20 @@ from player import ai, api, dummy, user
 __version__ = "0.1-Alpha"
 
 # usage argument parser: [-h] [-t | -g] [-p PLAYER PLAYER][-pT {User,AI,Player,Dummy} {User,AI,Player,Dummy}][-pD {0,1,2,3} {0,1,2,3}] [-v]
-parser = argparse.ArgumentParser()
+def parser_test():
+    parser = argparse.ArgumentParser()
+    return parser
+
+parser = parser_test()
 ui_group = parser.add_mutually_exclusive_group()
 ui_group.add_argument("-t", "--terminal", help="starts the terminal ui", action="store_true")
 ui_group.add_argument("-g", "--gui", help="starts the GUI", action="store_true")
+
 player_name_type_group = parser.add_argument_group()
 player_name_type_group.add_argument("-p", "--player", nargs=2, help=argparse.SUPPRESS) # help="set name player 1 and 2"
 player_name_type_group.add_argument("-pT","--player_type", nargs=2, choices=["User","AI", "Player", "Dummy"], help=argparse.SUPPRESS) # help="enter player type for player 1 and 2"
 player_name_type_group.add_argument("-pD","--player_difficulty", nargs=2, type=int, choices=range(0,4), help=argparse.SUPPRESS) # help="enter ai difficulty for player 1 and 2, use 0 if you are not using ai"
+
 parser.add_argument("-v", "--version", help="print the version number and exit", action="store_true")
 args = parser.parse_args()
 
@@ -34,25 +40,17 @@ def main():
         print("Start GUI")
     elif args.terminal:
         ui_status = 0
-        if not (args.player is None and args.player_type is None and args.player_difficulty is None):
-            interrogate_settings = []
-            for i in range(len(args.player)):
-                interrogate_setting = {
-                    "player_num": i, "player_name": args.player[i], "player_type": args.player_type[i], "player_difficulty": args.player_difficulty[i]}
-                interrogate_settings.append(interrogate_setting)
-
-            settings_ui = ui_switcher(ui_status).Settings()
-            player_settings = settings_ui.fast_interrogate_settings(interrogate_settings)
-            start_chess_master(ui_status, settings_ui, player_settings)
-        else:
-            settings_ui = ui_switcher(ui_status).Settings()
-            player_settings = settings_ui.interrogate_settings()
-            start_chess_master(ui_status, settings_ui, player_settings)
+        start_chess_master(ui_status)
     elif args.version:
         print(__version__)
 
-def start_chess_master(ui_status, settings_ui, player_settings):
+def start_chess_master(ui_status):
     try:
+        settings_ui = ui_switcher(ui_status).Settings()
+        if not (args.player is None and args.player_type is None and args.player_difficulty is None):
+            player_settings = settings_ui.interrogate_settings(args.player, args.player_type, args.player_difficulty)
+        else:
+            player_settings = settings_ui.interrogate_settings()
         players = []
         for player_setting in player_settings:
             type = type_switcher(player_setting.type)
