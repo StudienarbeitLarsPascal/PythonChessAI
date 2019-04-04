dd459de55fb5445b939a9cf163096ebb571e18a1
Lars Dittert <l.dittert@fensterbau-rueckriegel.de>
2019-04-04 14:54:52 +0200

    used training_dummy script to create history elements

--- python_chess_ai/training_dummy.sh
+++ python_chess_ai/training_dummy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ToDo: Argumentparser to specify which player_type should play against which_player_type
-for i in {1.."$1"}
+for i in {1..500}
 do
    python main.py -t --player DummLars DummPascal -pT Dummy Dummy -pD 0 0 <<<0
-   echo
+   echo $i
 done
